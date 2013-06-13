c196ea1899cd991ce852967aeb294fc8dc7585a4
Max <max@recoset.com>
2013-06-12 22:42:34 -0400

    temporary commit

--- game.py
+++ game.py
@@ -7,21 +7,21 @@ class Game:
         # list of players, currently only supporting two
         self.players = players
         self.turn = 0
-        self.moves = []
+        self.spawn_times = []
 
-        # Create the starting pieces for each player
         for i, player in enumerate(players):
-            # print player
+            # Set the home nodes for each player
             board.home_nodes[i].contents.set_owner(player)
+            # Create the starting pieces for each player
             for neighbor in board.home_nodes[i].dirs.values():
                 new_id = "{}{}".format(player.id, len(player.pieces))
                 p = Piece(player, new_id, 1, neighbor)
 
     def next_move(self):
-        moves = self.moves[self.turn]
+        moves = {}
         moves.update(player1.moves[self.turn])
         moves.update(player2.moves[self.turn])
-        
+
 
 
 
