309e7918cc84ab8452e5b55b627813692740093a
Max <max@recoset.com>
2013-06-12 22:04:18 -0400

    added move resolution phase logic

--- game.py
+++ game.py
@@ -9,6 +9,7 @@ class Game:
         self.turn = 0
         self.moves = []
         self.illegal_move_penalty = 10
+        self.movement_cooldown = 5
 
         # Create the starting pieces for each player
         for i, player in enumerate(players):
@@ -18,8 +19,11 @@ class Game:
                 new_id = "{}{}".format(player.id, len(player.pieces))
                 p = Piece(player, new_id, 1, neighbor)
 
-    def spawn_time(x):
-        return sum([i*6 for i in range(x)])/2 + x + 5 
+    def spawn_time(r):
+        return sum([i*6 for i in range(r)])/2 + r + 5 
+
+    def combine_time(r):
+        return (3+r) * 2
 
     def next_move(self):
         moves = self.moves[self.turn]
@@ -30,6 +34,7 @@ class Game:
             if order[0] == "move":
                 if p.can_move_to(order[1]):
                     p.move_to(order[1])
+                    p.cooldown = self.movement_cooldown
                 else:
                     p.cooldown += self.illegal_move_penalty
             elif order[0] == "spawn":
@@ -48,8 +53,26 @@ class Game:
                 self.resolve(loc)
 
     def resolve(loc):
-        pass
+        owners = set([item.owner for item in loc.contents])
+        if len(owners) > 1:
+            for item in loc.contents:
+                if type(item) == Home:
+                    end_game(item.owner)
+                else:
+                    del item
+        else:
+            new_range = 0
+            for item in loc.contents:
+                if type(item) == Piece:
+                    new_range += item.range() 
+                    del item
+            if new_range > 0:
+                p = owners.pop()
+                new_id = p.get_next_id
+                Piece(p, new_id, new_range, loc, self.combine_time(new_range))
 
+    def end_game(loser):
+        pass
 
 
 b = Board(40, 40)
