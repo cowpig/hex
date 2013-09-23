71111a7316ab86c91b3a8005b60a856dc4bbb17e
Max <max@recoset.com>
2013-09-22 21:13:43 -0400

    more to do on game state

--- user_interface/js/Grid.js
+++ user_interface/js/Grid.js
@@ -1,10 +1,29 @@
-var Board = Board || {};
+var Game = Game || {};
 
 // takes the output of a Hex board to initialize
-Board.Grid = function(width, height, starting_input) {
+Game.Board = function(width, height, starting_input) {
 	this.map = {};
 	for (var i=0; i<starting_input.length; i++){
 		var p = Hex.Point(input.coord[0], input.coord[1]);
-		this.map[p] = input.contents;
+		this.map[p] = {
+			'contents':input.contents
+		};
 	}
 };
+
+Game.Item = function(id) {
+	this.Id = id;
+}
+
+Game.Item.prototype.get_owner = function() {
+	if (this.Id.charAt(0) === "A")
+		return "A";
+	if (this.Id.charAt(0) === "B")
+		return "B";
+	return null;
+}
+
+// TODO: decide whether to encode everything in Id or create instance variables
+Game.Item.prototype.get_range = function() {
+	if (this.Id.charAt(0))
+}
