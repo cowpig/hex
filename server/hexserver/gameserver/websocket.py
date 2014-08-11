0b56432690e8683e279f8705b47d4ca0813020e2
Max <max@recoset.com>
2014-08-10 23:38:29 -0400

    websocket soccer

--- server/hexserver/gameserver/websocket.py
+++ server/hexserver/gameserver/websocket.py
@@ -7,28 +7,21 @@ from game_api import GameApi
 # todo : assign connection to a player
 
 class WSHandler(tornado.websocket.WebSocketHandler):
-	# def __init__(self):
-	# 	self.users = {}
-
 	def open(self):
-		# import pdb; pdb.set_trace()
-		print 'new connection'
-		self.api = GameApi(self.write_message)
-		print self.api.game.board
+		pass # do nothing, I think
 	
 	def on_message(self, message):
-		print 'message received %s' % message
-		return_statement = self.api.parse_input(message)
-		self.write_message(return_statement)
+		pass # it on to django's WSIG
 
 	def on_close(self):
-		print 'connection closed'
+		pass # a message to let django know
 
 
 application = tornado.web.Application([(r'/ws', WSHandler),])
 
 if __name__ == "__main__":
+	# connect to django's WSIG
 	http_server = tornado.httpserver.HTTPServer(application)
-	http_server.listen(8000)
+	http_server.listen(8000) # ?
 	tornado.ioloop.IOLoop.instance().start()
 
