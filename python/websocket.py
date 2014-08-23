3d3744cd4e05ea148d83b5705fb93fa8b3b0b6ee
Max <max@recoset.com>
2014-08-23 01:07:37 -0400

    plz

---
+++ python/websocket.py
@@ -0,0 +1,29 @@
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+import tornado.httpserver
+from game_api import GameApi
+
+# todo : assign connection to a player
+
+class WSHandler(tornado.websocket.WebSocketHandler):
+	def open(self):
+		print 'new connection'
+		self.api = GameApi(self.write_message)
+		print self.api.game.board
+	
+	def on_message(self, message):
+		print 'message received %s' % message
+		return_statement = self.api.parse_input(message)
+		self.write_message(return_statement)
+
+	def on_close(self):
+		print 'connection closed'
+
+
+application = tornado.web.Application([(r'/ws', WSHandler),])
+
+if __name__ == "__main__":
+	http_server = tornado.httpserver.HTTPServer(application)
+	http_server.listen(8000)
+	tornado.ioloop.IOLoop.instance().start()
