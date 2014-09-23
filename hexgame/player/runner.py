2c8f30d3b35dc2e3ab3c4b07d9cf1f7f62f1dafa
Surendran Mahendran <surenspost@gmail.com>
2014-09-23 11:47:17 -0400

    Debugging runner.py

--- hexgame/player/runner.py
+++ hexgame/player/runner.py
@@ -1,10 +1,14 @@
 execfile("player.py")
 
 if __name__ == "__main__":
+  print "I am starting up..."
 	while True:
+    print "I am in the while loop..."
 		game_state = raw_input()
 		if game_state.starts_with("game_over"):
 			break
 		print next_move(game_state)
+
+  print "I am exiting."
 
 	# signal rails to tear down docker
