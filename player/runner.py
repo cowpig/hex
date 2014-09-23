eeb4f65923577e89241f7a55054b95975902ab7e
Surendran Mahendran <surenspost@gmail.com>
2014-09-23 11:55:26 -0400

    Moving player files outside

--- player/runner.py
+++ player/runner.py
@@ -1,14 +1,14 @@
 execfile("player.py")
 
 if __name__ == "__main__":
-  print "I am starting up..."
+	print "I am starting up..."
 	while True:
-    print "I am in the while loop..."
+		print "I am in the while loop..."
 		game_state = raw_input()
 		if game_state.starts_with("game_over"):
 			break
 		print next_move(game_state)
 
-  print "I am exiting."
+	print "I am exiting."
 
 	# signal rails to tear down docker
